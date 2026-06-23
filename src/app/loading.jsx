48a0d2cd146f7c0f7f71c45d981354d26864f463
anas20023 <anasibnebelal400@gmail.com>
2026-06-23 20:51:17 +0600

    fix: Update Spinner color from accent to warning in Loading components

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -3,7 +3,7 @@ import { Spinner } from "@heroui/react";
 export default function Loading() {
     return (
         <div className="flex justify-center items-center min-h-screen">
-            <Spinner size="lg" color="accent" label="Loading..."  />
+            <Spinner size="lg" color="warning" label="Loading..."  />
         </div>
     );
 }
